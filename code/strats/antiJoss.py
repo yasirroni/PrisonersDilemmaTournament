605eef915c776b30127a0e4fffe2b6bf38778882
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2021-05-23 00:45:13 +0700

    add various agents

---
+++ code/strats/antiJoss.py
@@ -0,0 +1,12 @@
+
+import random
+
+# Joss but it forgives instead
+
+def strategy(history, memory):
+    choice = 1
+    if history.shape[1] >= 1 and history[1, -1] == 0:
+        choice = 0
+        if random.random() < 0.10:
+            choice = 1
+    return choice, None
