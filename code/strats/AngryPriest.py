350be60a8c1a856986d63408c25ba936c39b8ff9
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2021-05-22 21:47:54 +0700

    add comment to priest and angrypriest

--- code/strats/AngryPriest.py
+++ code/strats/AngryPriest.py
@@ -3,11 +3,20 @@ import numpy as np
 
 def forgivingCopycat(history):
     round = history.shape[1]
-    choice = 1
+    choice = 1 # default is play nice
     if history[1, -1] == 0:
+        # [
+        #   [X],
+        #   [0]
+        # ]
         choice = 0
     if round > 3 and choice == 0:
+        # forgive
         if history[0, -1] == 1 and history[0, -2] == 0 and history[1, -2] == 1:
+            # [
+            #   [0, 1],
+            #   [1, X]
+            # ]
             choice = 1
     return choice
 
